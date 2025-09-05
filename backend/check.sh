fb46543d344fd2364783ea53b44f65e7cb5bdf9c
dedenbangkit <mail@dedenbangkit.com>
2025-09-05 13:43:37 +0700

    [#1] Fix coveralls submission

--- backend/check.sh
+++ backend/check.sh
@@ -13,7 +13,7 @@ coverage report -m --rcfile=./.coveragerc
 coverage xml --rcfile=./.coveragerc
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
-  cd /app && coveralls
+  cd /app && COVERALLS_SERVICE_NAME=github-actions coveralls
 fi
 
 flake8
