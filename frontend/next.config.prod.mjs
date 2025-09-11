70a2aa1f99a87f084e15e027fd45ed25d2553701
dedenbangkit <mail@dedenbangkit.com>
2025-09-12 02:22:21 +0700

    [#9] Init Deployment

---
+++ frontend/next.config.prod.mjs
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const env = dotenv.config();
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
