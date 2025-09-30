b380ec35751b461448ce04a24a32b24fd4b1cc88
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-30 14:42:22 +0700

    [#21] apps: Wrap RootLayout in SQLiteProvider for database initialization

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,19 @@
 import { Stack } from "expo-router";
+import { SQLiteProvider, defaultDatabaseDirectory } from "expo-sqlite";
+import { DATABASE_NAME } from "@/database/config";
+import { migrateDbIfNeeded } from "@/database";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="home/index" options={{ headerShown: false }} />
-    </Stack>
+    <SQLiteProvider
+      databaseName={DATABASE_NAME}
+      directory={defaultDatabaseDirectory}
+      onInit={migrateDbIfNeeded}
+    >
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="home/index" options={{ headerShown: false }} />
+      </Stack>
+    </SQLiteProvider>
   );
 }
