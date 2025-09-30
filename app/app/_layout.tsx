b62d1b0ad40201d5c901dd80cf7554bc285a3c6c
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-30 18:49:52 +0700

    [#23] apps: refactor: Update RootLayout to improve stack navigation structure

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  anchor: "(tabs)/inbox",
+};
+
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="home/index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
