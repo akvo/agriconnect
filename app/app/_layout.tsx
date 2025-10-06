540d1ad74f1fdfcca013f354894a649dbd1fd8b3
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-06 17:48:45 +0700

    [#25] apps: feat(chat): add chat screen with HeaderOptions for ticket management

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Stack } from "expo-router";
 import { SQLiteProvider, defaultDatabaseDirectory } from "expo-sqlite";
 import { DATABASE_NAME } from "@/database/config";
 import { migrateDbIfNeeded } from "@/database";
 import { TicketProvider } from "@/contexts/TicketContext";
+import HeaderOptions from "@/components/chat/header-options";
 
 export const unstable_settings = {
   anchor: "(tabs)/inbox",
@@ -21,6 +23,22 @@ export default function RootLayout() {
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="login" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="chat"
+              options={({
+                navigation,
+                route,
+              }: {
+                navigation: any;
+                route: any;
+              }) => ({
+                headerShown: true,
+                headerTitleAlign: "left",
+                headerRight: () => (
+                  <HeaderOptions ticketID={route?.params?.ticketNumber} />
+                ),
+              })}
+            />
           </Stack>
         </TicketProvider>
       </AuthProvider>
