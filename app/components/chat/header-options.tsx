70a23e20baf8b630c7467505bdd75ad2b1d1a99e
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-07 10:21:34 +0700

    [#25] apps: refactor(chat): replace TouchableOpacity with DropdownMenu
    for closing tickets and streamline ticket closure logic

--- app/components/chat/header-options.tsx
+++ app/components/chat/header-options.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { TouchableOpacity, Alert } from "react-native";
+import { View, Alert } from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import Feathericons from "@expo/vector-icons/Feather";
 import { api } from "@/services/api";
 import { useAuth } from "@/contexts/AuthContext";
 import { dao } from "@/database/dao";
+import { DropdownMenu, MenuItem } from "../dropdown-menu";
+import { useTicket } from "@/contexts/TicketContext";
 
 type Props = {
   ticketID?: string | number;
@@ -12,35 +14,44 @@ type Props = {
 
 const HeaderOptions = ({ ticketID }: Props) => {
   const { user } = useAuth();
+  const { updateTicket } = useTicket();
   const db = useSQLiteContext();
 
-  const onYes = async () => {
+  const handleCloseTicket = async () => {
     try {
       const ticket = dao.ticket.findByTicketNumber(db, ticketID as string);
       if (!ticket) {
         throw new Error("Ticket not found");
       }
-      await api.closeTicket(user?.accessToken, ticket.id);
+      const { ticket: resData } = await api.closeTicket(
+        user?.accessToken,
+        ticket.id,
+      );
+      updateTicket(ticket.id, {
+        resolvedAt: resData.resolved_at,
+        resolvedBy: user.id,
+      });
     } catch (error) {
       console.error("Error closing ticket:", error);
     }
   };
 
+  const onCloseTicket = () => {
+    const id = ticketID ?? "unknown";
+    Alert.alert("Close Ticket", `Would you like to close this ticket: ${id}?`, [
+      { text: "Yes", onPress: handleCloseTicket },
+      { text: "No", style: "cancel" },
+    ]);
+  };
+
   return (
-    <TouchableOpacity
-      style={{ paddingHorizontal: 8, paddingVertical: 6 }}
-      onPress={() => {
-        const id = ticketID ?? "unknown";
-        Alert.alert("Options", `Would you like to close this ticket: ${id}?`, [
-          { text: "Yes", onPress: onYes },
-          { text: "No", style: "cancel" },
-        ]);
-      }}
-      accessibilityLabel="More options"
-      accessibilityRole="button"
-    >
-      <Feathericons name="check" size={22} color="black" />
-    </TouchableOpacity>
+    <View style={{ paddingHorizontal: 8, paddingVertical: 6 }}>
+      <DropdownMenu
+        trigger={<Feathericons name="more-vertical" size={22} color="black" />}
+      >
+        <MenuItem onPress={onCloseTicket}>Close Ticket</MenuItem>
+      </DropdownMenu>
+    </View>
   );
 };
 
