d47cb5164ae9883124b2b666cfa68e7249364ce9
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-13 12:04:31 +0700

    [#30] apps: fix unmatched routes when running in dev build
    feat(notfound): add NotFoundScreen component with navigation link to home
    feat(layout): update header title rendering in RootLayout component
    feat(index): create index file to redirect to home screen

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout() {
         <WebSocketProvider>
           <TicketProvider>
             <Stack>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="login" options={{ headerShown: false }} />
               <Stack.Screen
