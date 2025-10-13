87d5d6bd4ddaed9c2be8b996ce49d3f2e093c235
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-13 14:47:58 +0700

    [#30] apps: fix(notifications): simplify notification listener cleanup

--- app/contexts/NotificationContext.tsx
+++ app/contexts/NotificationContext.tsx
@@ -234,15 +234,8 @@ export const NotificationProvider: React.FC<{ children: ReactNode }> = ({
 
     return () => {
       tokenListener.remove();
-
-      if (notificationListener.current) {
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        );
-      }
-      if (responseListener.current) {
-        Notifications.removeNotificationSubscription(responseListener.current);
-      }
+      notificationListener.current?.remove();
+      responseListener.current?.remove();
     };
   }, []);
 
