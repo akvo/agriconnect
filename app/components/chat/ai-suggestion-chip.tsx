9113e0d1328ca42827cc59a7b7d61ee06c8edabb
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-22 12:32:59 +0700

    [#39] apps: enhance AISuggestionChip with dismiss functionality and edit icon

--- app/components/chat/ai-suggestion-chip.tsx
+++ app/components/chat/ai-suggestion-chip.tsx
@@ -6,6 +6,8 @@ import {
   ScrollView,
   StyleSheet,
 } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Feathericons from "@expo/vector-icons/Feather";
 import typography from "@/styles/typography";
 import themeColors from "@/styles/colors";
 
@@ -32,7 +34,39 @@ const AISuggestionChip: React.FC<AISuggestionChipProps> = ({
 
   return (
     <View style={[styles.suggestionContainer, containerStyle]}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <View style={styles.suggestionHeader}>
+        <View style={styles.suggestionIconTextContainer}>
+          <Ionicons
+            name="sparkles-outline"
+            size={16}
+            color={themeColors.textSecondary}
+          />
+          <Text style={styles.suggestionText}>AI suggestion</Text>
+        </View>
+        <View>
+          {/* Close button for dismiss */}
+          <TouchableOpacity onPress={() => onAccept("")}>
+            <Ionicons
+              name="close-sharp"
+              size={24}
+              color={themeColors.textSecondary}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        style={styles.scrollViewCard}
+      >
+        <View style={styles.editContainer}>
+          <View style={styles.editCircle}>
+            <Feathericons
+              name="edit"
+              size={16}
+              color={themeColors["green-500"]}
+            />
+          </View>
+        </View>
         <TouchableOpacity
           onPress={() => onAccept(suggestion)}
           style={styles.suggestionChip}
@@ -50,15 +84,52 @@ const styles = StyleSheet.create({
   suggestionContainer: {
     borderTopWidth: 1,
     borderColor: themeColors.mutedBorder,
-    paddingVertical: 8,
-    paddingHorizontal: 12,
+    paddingVertical: 16,
+    paddingHorizontal: 16,
     backgroundColor: themeColors.background,
   },
   suggestionChip: {
+    width: "100%",
     backgroundColor: themeColors["green-50"],
     padding: 8,
     borderRadius: 8,
     maxWidth: 300,
+  },
+  suggestionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 4,
+  },
+  suggestionIconTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  suggestionText: {
+    ...typography.label3,
+    color: themeColors.textSecondary,
+    fontWeight: 700,
+  },
+  scrollViewCard: {
+    borderWidth: 1,
+    borderColor: themeColors.mutedBorder,
+    borderRadius: 16,
+    padding: 12,
+  },
+  editContainer: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginBottom: 12,
+  },
+  editCircle: {
+    width: 32,
+    height: 32,
+    borderRadius: 48,
+    backgroundColor: themeColors["green-50"],
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 4,
   },
 });
 
