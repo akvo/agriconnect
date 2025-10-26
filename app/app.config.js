da749d8650dcc1b7a2aef6d188d05e8f0979193d
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-10-26 07:44:29 +0700

    [#40] apps: Update user interface style and navigation bar settings in app configuration

--- app/app.config.js
+++ app/app.config.js
@@ -7,7 +7,7 @@ module.exports = {
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "agriconnect",
-    userInterfaceStyle: "automatic",
+    userInterfaceStyle: "light",
     newArchEnabled: true,
     android: {
       package: "com.akvo.agriconnect",
@@ -41,10 +41,7 @@ module.exports = {
           image: "./assets/images/splash-icon.png",
           imageWidth: 200,
           resizeMode: "contain",
-          backgroundColor: "#ffffff",
-          dark: {
-            backgroundColor: "#000000",
-          },
+          backgroundColor: "#027E5D",
         },
       ],
       [
@@ -64,7 +61,9 @@ module.exports = {
       ],
     ],
     androidNavigationBar: {
-      visible: "immersive",
+      visible: "sticky-immersive",
+      barStyle: "light-content",
+      backgroundColor: "#000000",
     },
     experiments: {
       typedRoutes: true,
