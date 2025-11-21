244928f37c7fe0753289d454f83f892b526677ae
wayangalihpratama <wgprtm@gmail.com>
2025-11-21 15:43:14 +0800

    [#59] Fix alembic

--- backend/alembic/versions/2025_11_10_0458-be8cf7df4bef_add_kb_url_and_document_url_to_service_.py
+++ backend/alembic/versions/2025_11_10_0458-be8cf7df4bef_add_kb_url_and_document_url_to_service_.py
@@ -1,7 +1,7 @@
 """add kb_url and document_url to service_tokens table
 
 Revision ID: be8cf7df4bef
-Revises: a1b2c3d4e5f6
+Revises: b3d2a0414e18
 Create Date: 2025-11-06 09:07:49.961295
 """
 
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "be8cf7df4bef"
-down_revision: Union[str, None] = "a1b2c3d4e5f6"
+down_revision: Union[str, None] = "b3d2a0414e18"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
