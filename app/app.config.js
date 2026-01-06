fd8d627f87fc62732cc45b2efe9c69d80cfa9016
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-01-06 14:03:24 +0700

    [#93] apps: Bump version to 1.2.4 in app configuration

--- app/app.config.js
+++ app/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "agriconnect",
     slug: "agriconnect",
-    version: "1.2.3",
+    version: "1.2.4",
     owner: "akvo",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
